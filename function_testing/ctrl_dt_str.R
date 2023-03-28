f1665fc24576b1e3a6c07e05fe5c30bcc4dcff04
Thea B <abourdea@med.umich.edu>
2023-03-28 15:54:44 -0400

    add p chart alerts, colors for plotting

--- function_testing/ctrl_dt_str.R
+++ function_testing/ctrl_dt_str.R
@@ -5,14 +5,15 @@ library(magrittr)
 library(qicharts2)
 library(qcc)
 library(data.table)
+library(OBI.color)
 
 obi = data.table::fread("P:/OBI_abstracted_data/Current_Data/data/output/sourcetables_OBI_export_recodes.csv")
 
 structure_data = function(df,
-                       date_var,
-                       date_gran = c(year_mon, year_qtr),
-                       num_var,
-                       den_var) {
+                          date_var,
+                          date_gran = c(year_mon, year_qtr),
+                          num_var,
+                          den_var) {
   ctrl_cohort = df %>% filter(flg_complete == 1) %>% mutate(
     date_lub = lubridate::dmy_hms({
       {
@@ -41,7 +42,7 @@ structure_data = function(df,
     .groups = "drop"
   )
 
-# qicharts2 package to get CL frozen @ 12 mo ------------------------------
+  # qicharts2 package to get CL frozen @ 12 mo ------------------------------
 
   limits_pre = qic(
     num,
@@ -79,12 +80,40 @@ structure_data = function(df,
   ctrl_cohort_fin = cbind(ctrl_w_CL, limits)
 
   # apply shift violations to prior rows ------------------------------------
-  ctrl_cohort_fin = ctrl_cohort_fin %>% mutate(x3_sig_viol = ifelse(rate > UCL, 1, 0),
-                                               n_pts_oneside_CL = ifelse(rate > CL, 1, 0))
+  ctrl_cohort_fin = ctrl_cohort_fin %>% mutate(
+    x3_sig_viol = ifelse(rate > UCL, 1, 0),
+    n_pts_oneside_CL = ifelse(rate > CL, 1, 0)
+  )
 
   ## make data.table and assign values for shift violations
   ctrl_cohort_fin = setDT(ctrl_cohort_fin)
   ctrl_cohort_fin[, rleid_pts := sum(n_pts_oneside_CL), by = rleid(n_pts_oneside_CL)]
+
+  # final data manipulation -------------------------------------------------
+  ## apply violations to N prior data points, note if point is above UCL or
+  ## below LCL, apply colors for ggplot
+
+  ctrl_cohort_alerts = ctrl_cohort_fin %>% mutate(
+    violations = ifelse(x3_sig_viol == 1, 1, ifelse(rleid_pts >= 8, 4, 0)),
+    above_or_below = ifelse(
+      violations == 1,
+      ifelse(rate > UCL, "Above", "Below"),
+      as.character(NA)
+    ),
+    p_chart_alert = case_when(
+      violations == 1 & above_or_below == "Above" ~ "Above UCL",
+      violations == 1 &
+        above_or_below == "Below" ~ "Below LCL",
+      violations == 4 ~ "Shift",
+      TRUE ~ "No alert"
+    ),
+    p_chart_color = case_when(
+      p_chart_alert == "Above UCL" ~ OBI.color::prim_pink(),
+      p_chart_alert == "Below LCL" ~ OBI.color::prim_teal(),
+      p_chart_alert == "Shift" ~ "#f8b434",
+      p_chart_alert == "No alert" ~ OBI.color::prim_dark_blue()
+    )
+  ) %>% select(-c(x3_sig_viol:above_or_below))
 
 }
 
