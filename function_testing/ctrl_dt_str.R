8e3063887fc3ba0838c4efbe75dddd5036118183
Thea B <abourdea@med.umich.edu>
2023-03-28 14:21:33 -0400

    data structure, no ctrl chart parts

--- function_testing/ctrl_dt_str.R
+++ function_testing/ctrl_dt_str.R
@@ -5,16 +5,25 @@ library(magrittr)
 
 obi = data.table::fread("P:/OBI_abstracted_data/Current_Data/data/output/sourcetables_OBI_export_recodes.csv")
 
-ctrl_dt_str = function(df, date_var) {
-  df = df %>% filter(flg_complete == 1) %>% mutate(
+ctrl_dt_str = function(df,
+                       date_var,
+                       date_gran = c(year_mon, year_qtr),
+                       num_var,
+                       den_var) {
+  df %>% filter(flg_complete == 1) %>% mutate(
     date_lub = lubridate::dmy_hms({
       {
         date_var
       }
     }),
     year_mon = zoo::as.yearmon(date_lub),
-    year_qtr = zoo::as.yearqtr(date_lub)
-  ) %>% group_by(year_mon) %>% summarize(
+    year_qtr = zoo::as.yearqtr(date_lub),
+    date_var = {
+      {
+        date_gran
+      }
+    }
+  ) %>% group_by(date_var) %>% summarize(
     num = sum({
       {
         num_var
@@ -30,5 +39,4 @@ ctrl_dt_str = function(df, date_var) {
   )
 }
 
-test = obi %>% test_fun(infant_dob_dt, cesarean, birth)
-
+test_dt = obi %>% ctrl_dt_str(infant_dob_dt, year_mon, cesarean, birth)
