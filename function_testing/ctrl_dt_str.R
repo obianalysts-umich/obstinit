033f6f8e850b65ab9ba23fe851cac85a1f9f7908
Thea B <abourdea@med.umich.edu>
2023-03-28 15:12:13 -0400

    add CL from qicharts2 package

--- function_testing/ctrl_dt_str.R
+++ function_testing/ctrl_dt_str.R
@@ -2,15 +2,16 @@
 library(tidyverse)
 library(zoo)
 library(magrittr)
+library(qicharts2)
 
 obi = data.table::fread("P:/OBI_abstracted_data/Current_Data/data/output/sourcetables_OBI_export_recodes.csv")
 
-ctrl_dt_str = function(df,
+structure_data = function(df,
                        date_var,
                        date_gran = c(year_mon, year_qtr),
                        num_var,
                        den_var) {
-  df %>% filter(flg_complete == 1) %>% mutate(
+  ctrl_cohort = df %>% filter(flg_complete == 1) %>% mutate(
     date_lub = lubridate::dmy_hms({
       {
         date_var
@@ -37,6 +38,27 @@ ctrl_dt_str = function(df,
     rate = num / denom,
     .groups = "drop"
   )
+
+# qicharts2 package to get CL frozen @ 12 mo ------------------------------
+
+  limits_pre = qic(
+    num,
+    n = denom,
+    x = date_var,
+    freeze = 12,
+    data = ctrl_cohort,
+    chart    = 'p'
+  )
+
+  ## get CL
+  CL_pre = summary(limits_pre)[, 12]
+
+  ## rep values
+  CL = rep(CL_pre, nrow(ctrl_cohort))
+
+  ## bind to original
+  ctrl_w_CL = cbind(ctrl_cohort, CL)
+
 }
 
-test_dt = obi %>% ctrl_dt_str(infant_dob_dt, year_mon, cesarean, birth)
+test_dt = obi %>% structure_data(infant_dob_dt, year_mon, cesarean, birth)
