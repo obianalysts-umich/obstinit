76fbc7d494bb5c3119066f593f1a8f46dfe471dd
Thea B <abourdea@med.umich.edu>
2023-03-28 15:18:23 -0400

    add UCL, LCL, violations

--- function_testing/ctrl_dt_str.R
+++ function_testing/ctrl_dt_str.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(zoo)
 library(magrittr)
 library(qicharts2)
+library(qcc)
+library(data.table)
 
 obi = data.table::fread("P:/OBI_abstracted_data/Current_Data/data/output/sourcetables_OBI_export_recodes.csv")
 
@@ -59,6 +61,31 @@ structure_data = function(df,
   ## bind to original
   ctrl_w_CL = cbind(ctrl_cohort, CL)
 
+  # use qcc package to get limits -------------------------------------------
+
+  qc_limits = qcc(
+    type = "p",
+    data = ctrl_w_CL$num,
+    sizes = ctrl_w_CL$denom,
+    center = ctrl_w_CL$CL,
+    plot = F
+  )
+
+  # get limits
+  limits = qc_limits$limits
+  rownames(limits) = c(1:nrow(limits))
+
+  # bind limits to main dataset
+  ctrl_cohort_fin = cbind(ctrl_w_CL, limits)
+
+  # apply shift violations to prior rows ------------------------------------
+  ctrl_cohort_fin = ctrl_cohort_fin %>% mutate(x3_sig_viol = ifelse(rate > UCL, 1, 0),
+                                               n_pts_oneside_CL = ifelse(rate > CL, 1, 0))
+
+  ## make data.table and assign values for shift violations
+  ctrl_cohort_fin = setDT(ctrl_cohort_fin)
+  ctrl_cohort_fin[, rleid_pts := sum(n_pts_oneside_CL), by = rleid(n_pts_oneside_CL)]
+
 }
 
-test_dt = obi %>% structure_data(infant_dob_dt, year_mon, cesarean, birth)
+test_dt = obi %>% structure_data(infant_dob_dt, year_qtr, cesarean, birth)
