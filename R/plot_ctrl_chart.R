0d2c794833dc520af59e1e0366bad3241b5b8711
Thea B <abourdea@med.umich.edu>
2023-04-04 14:02:33 -0400

    remove legend for line
    plotting is working correctly at this stage with the correct color gradients and lines. Mapping of legend is still incorrect.

--- R/plot_ctrl_chart.R
+++ R/plot_ctrl_chart.R
@@ -61,7 +61,7 @@ plot_ctrl_chart = function(df, increase_is_bad = T) {
     scale_fill_identity(guide = guide_legend("Control chart alert"),
                         labels = levels(factor(df$p_chart_alert))) +
     geom_link2(aes(y = rate, color = line_value), linewidth = 0.8) +
-    scale_color_gradientn(colors = line_color_pal, values = scales::rescale(line_values)) +
+    scale_color_gradientn(colors = line_color_pal, values = scales::rescale(line_values), guide = "none") +
     theme_bw() +
     scale_y_continuous(labels = scales::percent)
   
