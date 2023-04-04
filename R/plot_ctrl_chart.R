0b392f87b2ea1ce6dedd9d346a3abcfa33039373
Thea B <abourdea@med.umich.edu>
2023-04-04 09:18:54 -0400

    update plot_ctrl_chart for gradient

--- R/plot_ctrl_chart.R
+++ R/plot_ctrl_chart.R
@@ -2,58 +2,78 @@
 #' Plot control chart
 #'
 #' @param df A data frame in wide format, created by the structure_data function
+#' @param increase_is_bad If TRUE, this is a trend that would ideally be decreasing over time; if FALSE, ideally increasing
 #' @import data.table
 #' @export
 #' @rdname plot_ctrl_chart
 
 
-plot_ctrl_chart = function(df) {
+plot_ctrl_chart = function(df, increase_is_bad = T) {
   
-  # assign color based on value
-  line_color <-
-    c("No alert" = OBI.color::prim_dark_blue(),
-      "Shift" = "#f8b434")
+  # assign line color based on value
   
-  dot_color_df <- df %>%
-    distinct(p_chart_alert, point_color)
-  dot_color <- dot_color_df$point_color
-  names(dot_color) <- dot_color_df$p_chart_alert
+  line_color_pal = c(if (increase_is_bad &
+                         any(df$p_chart_alert == "Below UCL")) {
+    OBI.color::prim_teal()
+  },
+  if (increase_is_bad == F &
+      any(df$p_chart_alert == "Below UCL")) {
+    "#b64083"
+  },
+  if (any(df$p_chart_alert == "Shift")) {
+    "#f8b434"
+  },
+  if (increase_is_bad &
+      any(df$p_chart_alert == "Above UCL")) {
+    "#b64083"
+  },
+  if (increase_is_bad == F &
+      any(df$p_chart_alert == "Below UCL")) {
+    OBI.color::prim_teal()
+  })
+  
+  # line_color <-
+  #   c("No alert" = OBI.color::prim_dark_blue(),
+  #     "Shift" = "#f8b434")
+  
+  # dot_color_df <- df %>%
+  #   distinct(p_chart_alert, point_color)
+  # dot_color <- dot_color_df$point_color
+  # names(dot_color) <- dot_color_df$p_chart_alert
   
   # plot --------------------------------------------------------------------
   
   ggplot(aes(x = date_var),
          data = df) +
-    geom_ribbon(
-      aes(ymin = LCL, ymax = UCL),
-        fill = "#CAC4CE",
-        alpha = 0.3
-      ) +
-        geom_line(
-          aes(y = CL),
-          color = OBI.color::prim_dark_blue(),
-          linetype = "dashed",
-          linewidth = 0.75,
-          alpha = 0.5
-        ) +
-        geom_line(aes(
-          y = rate,
-          color = as.factor(shift_line),
-          group = 1
-        ),
-        linewidth = 0.8) +
-        scale_colour_manual(values = line_color,
-                            guide = guide_legend(NULL)) +
-        geom_point(
-          aes(y = rate, fill = p_chart_alert),
-          size = 3,
-          shape = 21,
-          stroke = 0
-        ) +
-        scale_fill_manual(
-          values = dot_color,
-          guide = guide_legend("Control chart alert", reverse = T)
-        ) +
-        theme_bw() +
-        scale_y_continuous(labels = scales::percent)
-      
+    geom_ribbon(aes(ymin = LCL, ymax = UCL),
+                fill = "#CAC4CE",
+                alpha = 0.4) +
+    geom_line(
+      aes(y = CL),
+      color = OBI.color::prim_dark_blue(),
+      linetype = "dashed",
+      linewidth = 0.75,
+      alpha = 0.5
+    ) +
+    # geom_line(aes(
+    #   y = rate,
+    #   color = as.factor(shift_line),
+    #   group = 1
+    # ),
+    #linewidth = 0.8) +
+    # scale_colour_manual(values = line_color,
+    #                     guide = guide_legend(NULL)) +
+    geom_point(
+      aes(y = rate, fill = point_color),
+      size = 3,
+      shape = 21,
+      stroke = 0
+    ) +
+    scale_fill_identity(guide = guide_legend("Control chart alert", reverse = T),
+                        labels = levels(factor(df$p_chart_alert))) +
+    geom_link2(aes(y = rate, color = line_value), linewidth = 0.8) +
+    scale_color_gradientn(colors = line_color_pal) +
+    theme_bw() +
+    scale_y_continuous(labels = scales::percent)
+  
 }
