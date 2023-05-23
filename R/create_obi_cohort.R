1ce36e812b69e39e9e319623ec81cc49ea124774
Thea B <abourdea@med.umich.edu>
2023-05-23 08:14:11 -0400

    create_obi_cohort

---
+++ R/create_obi_cohort.R
@@ -0,0 +1,26 @@
+
+#' Create OBI cohort of locked, complete cases for most analyses
+#' 
+#' @param df Data frame -- usually this is the data frame read into R using data.table::fread
+#' @import tidyverse
+#' @export
+#' @rdname create_obi_cohort
+
+create_obi_cohort = function(df) {
+  sys_lock_dt_90 = format(Sys.Date() - lubridate::days(91), "%m/%d/%Y")
+  
+  obi_cohort = df %>% mutate(
+    infant_dob_dt = lubridate::dmy_hms(infant_dob_dt),
+    case_lock_dt = data.table::fifelse(
+      infant_dob_dt >= lubridate::ymd_hms("2023-01-01 00:00:00"),
+      infant_dob_dt + days(90),
+      infant_dob_dt + days(120)
+    ),
+    case_locked = ifelse(case_lock_dt < Sys.Date(), 1, 0)
+  ) %>% filter(
+    flg_complete == 1,
+    case_locked == 1,
+    infant_dob_dt >= lubridate::ymd_hms("2020-01-01 00:00:00")
+  )
+  
+}
