49ede6ea87933c184a3262ecef36495f5241ff70
abourdea <112494834+abourdea@users.noreply.github.com>
2023-09-01 12:17:54 -0400

    finalize function

--- R/sites_w_all_years_dt.R
+++ R/sites_w_all_years_dt.R
@@ -1,11 +1,15 @@
 
-#' Limit dataset to hospitals that have data for all years 2020 - current
+#' Limit dataset to hospitals that have data for all years 2020 - max year of your choice
 #' 
-#' @param df Data frame -- usually this is OBI's current, processed data read into R using data.table::fread
+#' @param df Data frame -- usually this is OBI's current, processed data read into R using data.table::fread. Preferably this has already been run through obstinit::create_obi_cohort()
 #' @import tidyverse
 #' @export
 #' @rdname sites_w_all_years_dt
 
 sites_w_all_years_dt = function(df, max_year) {
-  df %>% filter(birth_year > 2019, birth_year <= max_year) %>% distinct(site_name, birth_year)
+  n_years = (max_year - 2020) + 1
+  
+  sites = df %>% filter(birth_year > 2019, birth_year <= max_year) %>% distinct(site_name, birth_year) %>% add_count(site_name) %>% arrange(site_name) %>% filter(n == n_years) %>% distinct(site_name) %>% pull()
+  
+  df %>% filter(site_name %in% sites) 
 }
