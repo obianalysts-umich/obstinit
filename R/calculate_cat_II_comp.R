5a5efa7458c7ee22caba47cb2876c6bfc723179e
Thea B <abourdea@med.umich.edu>
2023-09-20 13:52:21 -0400

    add function for cat II compliance

---
+++ R/calculate_cat_II_comp.R
@@ -0,0 +1,17 @@
+
+#' Calculate cat II compliance
+#'
+#' @export
+#' @param df Data frame -- usually this is the data frame read into R using data.table::fread
+#' @import tidyverse
+#' @rdname read_current_data
+
+calculate_cat_II_comp = function(df) {
+  df %>% filter(mode_of_delivery_cd == 4,
+                ces_primary_indication_cd == 6,
+                fht_category_e %in% c(1, 3)) %>% summarize(
+                  cat_II_num = sum(ifelse(fht_mgt_documentation_e == 1, 1, 0)),
+                  cat_II_denom = sum(birth),
+                  cat_II_comp_rate = cat_II_num / cat_II_denom
+                )
+}
