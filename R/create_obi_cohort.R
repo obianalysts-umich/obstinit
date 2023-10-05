ba740498e6b7a654700d707771688f469185757a
Xilin Chen <xilinc@med.umich.edu>
2023-10-05 13:07:02 -0400

    create_obi_cohort
    clean code and add descriptions

--- R/create_obi_cohort.R
+++ R/create_obi_cohort.R
@@ -1,23 +1,24 @@
 
 #' Create OBI cohort of locked, complete cases for most analyses
+#' @description
+#' this function does 
+#'    1. include case ≥2020-01-01 based on infant date of birth
+#'    2. include case if case is complete using flg_complete
+#'    3. create case_locked varaible using 90 days lock
+#'    4. filter to complete and locked cases(limit_to_locked = TRUE)
+#' 
 #' 
 #' @param df Data frame -- usually this is the data frame read into R using data.table::fread
+#' @param limit_to_locked Logical -- if TRUE, only return cases that are locked
 #' @import tidyverse
 #' @export
-#' @rdname create_obi_cohort
 
-create_obi_cohort = function(df, limit_to_locked = T) {
-  sys_lock_dt_90 = format(lubridate::today() - lubridate::days(91), "%m/%d/%Y")
-  sys_lock_dt_120 = format(lubridate::today() - lubridate::days(121), "%m/%d/%Y")
-  
+create_obi_cohort = function(df, 
+                             limit_to_locked = T) {
   df1 = df %>% mutate(
     infant_dob_dt = lubridate::dmy_hms(infant_dob_dt),
     #case locks at MIDNIGHT AFTER THIS DATE
-    case_lock_dt = data.table::fifelse(
-      infant_dob_dt >= lubridate::ymd_hms("2023-01-01 00:00:00"),
-      lubridate::date(infant_dob_dt) + days(90),
-      lubridate::date(infant_dob_dt) + days(120)
-    ),
+    case_lock_dt = lubridate::date(infant_dob_dt) + days(90),
     case_locked = ifelse(case_lock_dt < lubridate::today(), 1, 0)
   )
   
