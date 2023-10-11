14c16f582ac8c811b435722a9786e665611fa3b5
Xilin Chen <xilinc@med.umich.edu>
2023-10-11 12:59:43 -0400

    error message fix and syntax

--- R/read_current_data.R
+++ R/read_current_data.R
@@ -1,11 +1,10 @@
-
 #' Read current OBI data
 #' @description
 #' read OBI analytic data from Turbo
 #' @export
 #' @rdname read_current_data
 
-read_current_data = function() {
+read_current_data <- function() {
   if (.Platform$OS.type == "windows") {
     # windows file path
     current_dt_path <-
@@ -15,19 +14,20 @@ read_current_data = function() {
     current_dt_path <-
       "/Volumes/nur-kanelow/OBI_abstracted_data/Current_Data/data/output/sourcetables_OBI_export_recodes.csv"
   }
-  
-  
+
+
   # test path
-  if(!file.exists(current_dt_path)){
-    error("Check your VPN connection. Path doesn't exsit at ", current_dt_path)
-  } 
-  
-  dt = data.table::fread(current_dt_path)
-  
+  if (!file.exists(current_dt_path)) {
+    stop("Check your VPN connection. Path doesn't exsit at ", current_dt_path)
+  }
+
+  dt <- data.table::fread(current_dt_path)
+
   # message after done reading
-  message("Current OBI data are loaded. The data are updated on ", 
-          lubridate::as_date(file.info(current_dt_path)$mtime), ".")
-  
+  message(
+    "Current OBI data are loaded. The data are updated on ",
+    lubridate::as_date(file.info(current_dt_path)$mtime), "."
+  )
+
   return(dt)
-  
 }
