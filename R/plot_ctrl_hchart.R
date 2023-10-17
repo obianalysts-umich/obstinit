e02a503b7ae124f2880167dd75d8659f69591fa8
Xilin Chen <xilinc@med.umich.edu>
2023-10-17 16:52:08 -0400

    lintr syntax

--- R/plot_ctrl_hchart.R
+++ R/plot_ctrl_hchart.R
@@ -1,19 +1,22 @@
-
 #' Plot interactive ctrl chart using highcharter package
-#' 
+#'
 #' @param df A data frame - should be in the structure of obstinit::structure_data
 #' @param title Title for plot
 #' @export
 #' @rdname plot_ctrl_hchart
 #' @import tidyverse
 
-plot_ctrl_hchart = function(df, title, ymin = NA, ymax = NA) {
-  plot_1 = highcharter::hchart(df,
-                               "arearange",
-                               hcaes(x = date_var, low = LCL, high = UCL)) %>%
-    highcharter::hc_add_series(df,
-                               "line",
-                               hcaes(x = date_var, y = rate, color = point_color)) %>%
+plot_ctrl_hchart <- function(df, title, ymin = NA, ymax = NA) {
+  plot_1 <- highcharter::hchart(
+    df,
+    "arearange",
+    hcaes(x = date_var, low = LCL, high = UCL)
+  ) %>%
+    highcharter::hc_add_series(
+      df,
+      "line",
+      hcaes(x = date_var, y = rate, color = point_color)
+    ) %>%
     highcharter::hc_add_series(
       df,
       "line",
@@ -35,23 +38,22 @@ plot_ctrl_hchart = function(df, title, ymin = NA, ymax = NA) {
         marker = list(symbol = "circle", radius = 5),
         tooltip = list(pointFormat = "<b>Alert: {point.p_chart_alert}</b> <br> <b>Rate</b>: {point.rate}% <br> <b>Num:</b> {point.num} <br> <b>Denom:</b> {point.denom}")
       )
-    ) %>% highcharter::hc_title(text = {
-      {
-        title
-      }
-    })
-  
+    ) %>%
+    highcharter::hc_title(text = {{ title }})
+
   ## fix x axis formats
-  
-  if(class(df$date_var) == "Date"){plot_1 %>%
+
+  if (class(df$date_var) == "Date") {
+    plot_1 %>%
       highcharter::hc_xAxis(
         type = "datetime",
         dateTimeLabelFormats = list(day = "%b %Y"),
         title = list(text = "")
-      )}
-  else(plot_1 %>%
-         highcharter::hc_xAxis(
-           title = list(text = "")
-         ))
-  
+      )
+  } else {
+    (plot_1 %>%
+      highcharter::hc_xAxis(
+        title = list(text = "")
+      ))
+  }
 }
