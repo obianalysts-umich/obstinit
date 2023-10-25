0d620a6effa8b6b5d28d21b307cac7542d26296f
Thea B <abourdea@med.umich.edu>
2023-10-25 15:43:22 -0400

    minor

--- R/calculate_TB_comp.R
+++ R/calculate_TB_comp.R
@@ -7,17 +7,20 @@
 #' @rdname calculate_TB_comp
 
 calculate_TB_comp = function(df) {
-  df %>% filter(planned_mode_of_delivery_cd == 1) %>% mutate(
-    huddle_per_12h = ifelse(
-      huddle_doc_b == 1,
-      (huddle_doc_count_no / Admission_minus_delivery) * 12,
-      0
-    ),
-    tb_full_compliance = ifelse(admit_huddle_mtg_compliance == 1 &
-                                  huddle_per_12h >= 1, 1, 0)
-  ) %>% summarize(
-    TB_num = sum(tb_full_compliance, na.rm = T),
-    TB_denom = sum(birth),
-    TB_comp_rate = TB_num / TB_denom
-  )
+  df %>%
+    filter(planned_mode_of_delivery_cd == 1) %>%
+    mutate(
+      huddle_per_12h = ifelse(
+        huddle_doc_b == 1,
+        (huddle_doc_count_no / Admission_minus_delivery) * 12,
+        0
+      ),
+      tb_full_compliance = ifelse(admit_huddle_mtg_compliance == 1 &
+                                    huddle_per_12h >= 1, 1, 0)
+    ) %>%
+    summarize(
+      TB_num = sum(tb_full_compliance, na.rm = T),
+      TB_denom = sum(birth),
+      TB_comp_rate = TB_num / TB_denom
+    )
 }
