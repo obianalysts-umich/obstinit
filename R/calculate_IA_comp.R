0b901660437f88055ba4dd895d90c87d3a2e7218
Thea B <abourdea@med.umich.edu>
2023-10-25 12:17:11 -0400

    update IA cop function criteria
    Drop cases where admit fetal monitoring type is unknown/missing ("{99}"). Will want to reverse this in 2024.

--- R/calculate_IA_comp.R
+++ R/calculate_IA_comp.R
@@ -7,9 +7,16 @@
 #' @rdname calculate_IA_comp
 
 calculate_IA_comp = function(df) {
-  df %>% filter(admit_labor_status_cd %in% c(3, 4),!ia_not_ordered_reason_e %in% c(1:6, 9:11)) %>% summarize(
-    IA_num = sum(sum(admit_fm_IA, na.rm = T)),
-    IA_denom = sum(birth),
-    IA_comp_rate = IA_num / IA_denom
-  )
+  df %>%
+    mutate(to_drop = ifelse(ia_not_ordered_reason_e %in% c(1:6, 9:11), 1, 0)) %>%
+    filter(
+      admit_labor_status_cd %in% c(3, 4, 5),
+      admit_fetal_monitor_type_e != "{99}",
+      to_drop != 1
+    ) %>%
+    summarize(
+      IA_num = sum(admit_fm_IA, na.rm = T),
+      IA_denom = sum(birth),
+      IA_comp_rate = IA_num / IA_denom
+    )
 }
