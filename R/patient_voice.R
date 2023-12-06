182d0636be32556149f34da6e78cb7870093e736
xilin_chen <xilinc@med.umich.edu>
2023-12-06 16:54:12 -0500

    create PRO cohort

--- R/patient_voice.R
+++ R/patient_voice.R
@@ -1,75 +1,71 @@
-# patient voice related functions
+# patient voice related functions 
 
 
-#' create patient voice response data
-#' @description
-#' this function creates a data frame excluding opt out patients
-#' - opt out from CDA
-#' - opt out from patients
-#' - limited to infant dob > 2023-5-10 (when PRO survey started)
-#'
+#' create patient voice response data 
+#'  
+#' @description 
+#' combine patient contact log with survey response data 
+#' - cohort: pts who received pro survey emails 
+#' - added var:  
+#'    - pro_response: pts who completed pro survey (opt out pts also consider complete) 
+#'    - pts choose to opt out: survey_informed_consent_b_0 ==0 
+#' 
 #' @param obi_dt OBI nightly export data
-#' @param pro_survey_expired logical, if TRUE, only include patients who are after 16 weeks postpartum
-#'
-#' @return a data frame with variables: patientid, site_name, infant_dob_dt, pro_response
-#' @export
-#'
-#' @examples
-#' obi_dt <- read_current_data() |>
-#'   create_obi_cohort(limit_to_locked = FALSE)
-#' create_pv_response_dt(obi_dt)
-#' @details
-#' this function read in pro survey data from `P:/OBI_abstracted_data/Current_Data/data/input/pro_6_week_postpartum.csv`
-#'
+#' @param pro_survey_expired logical, if TRUE, only include patients who are after 12 weeks postpartum 
+#' 
+#' @return a data frame with variables: patientid, site_name, infant_dob_dt, complete_pro_survey_flg, survey_informed_consent_b_0 
+#' @export 
+#' 
+#' @examples 
+#' obi_dt <- read_current_data() |> 
+#'   create_obi_cohort(limit_to_locked = FALSE) 
+#' create_pv_response_dt(obi_dt) 
+#'  
+#' @details 
+#' this function read in pro survey data from  
+#' - survey response: `P:/OBI_abstracted_data/Current_Data/data/input/pro_6_week_postpartum.csv` 
+#' - contact log pts who received emails: `P:/OBI_abstracted_data/Current_Data/data/input/contactlog.csv` 
+#' 
 
-create_pv_response_dt <- function(
-    obi_dt,
-    pro_survey_expired = FALSE) {
-  # PRO survey starting date
-  start_date <- lubridate::as_date("2023-5-10")
-  pro_expire_date <- lubridate::as_date(Sys.Date() - 16 * 7) # 16 weeks postpartum
-
-  # Patient voice data
-  if (Sys.info()["sysname"] == "Windows") {
-    pro_survey_dt <- data.table::fread("P:/OBI_abstracted_data/Current_Data/data/input/pro_6_week_postpartum.csv")
-  } else if (Sys.info()["sysname"] == "Darwin") {
-    pro_survey_dt <- data.table::fread("/Volumes/nur-kanelow/OBI_abstracted_data/Current_Data/data/input/pro_6_week_postpartum.csv")
-  }
-
-  # exclude opt out pts -----------------------------------------------------
-
-  ## 1. pts choose to opt out survey
-  pt_opt_out_list <-
-    pro_survey_dt[survey_informed_consent_b_0 == 0]$patientid
-
-  ## 2. CDA decide these pts should not receive survey
-  cda_opt_out_list <- obi_dt[pro_opt_out_e == 1]$patientid
-
-  # combine
-  opt_out_list <- c(pt_opt_out_list, cda_opt_out_list)
-
-  # response rate -----------------------------
-  # responded pts
-  pro_pt_ids <- pro_survey_dt %>%
-    filter(!patientid %in% opt_out_list) |>
-    select(patientid) %>%
-    mutate(pro_response = 1)
-
-  # add variable pro_response flag to obi_dt
-  obi_dt_pro_flg <- obi_dt %>%
-    filter(!patientid %in% opt_out_list) |>
-    left_join(pro_pt_ids, by = c("patientid")) %>%
-    mutate(pro_response = ifelse(is.na(pro_response), 0, 1)) %>%
-    filter(infant_dob_dt >= start_date) |>
-    select(patientid, site_name, infant_dob_dt, pro_response)
-
-  message("Patient voice survey started on ", start_date, ". Response rates for 2023 are calculated after the start date. ")
+create_pv_response_dt <- function(obi_dt, 
+                                  pro_survey_expired = FALSE) { 
+  # PRO survey starting date 
+  start_date <- lubridate::as_date("2023-5-10") 
+  pro_expire_date <- lubridate::as_date(Sys.Date() - 12 * 7) # 12 weeks postpartum 
   
-
-  if (pro_survey_expired == TRUE) {
-    obi_dt_pro_flg |>
-      filter(infant_dob_dt <= pro_expire_date)
-  } else {
-    obi_dt_pro_flg
-  }
-}
+  # Patient voice data 
+  if (Sys.info()["sysname"] == "Windows") { 
+    pro_survey_dt <- data.table::fread("P:/OBI_abstracted_data/Current_Data/data/input/pro_6_week_postpartum.csv") 
+    contact_log <- read_csv("P:/OBI_abstracted_data/Current_Data/data/input/contactlog.csv") 
+    
+  } else if (Sys.info()["sysname"] == "Darwin") { 
+    pro_survey_dt <- data.table::fread("/Volumes/nur-kanelow/OBI_abstracted_data/Current_Data/data/input/pro_6_week_postpartum.csv") 
+    contact_log <- read_csv("/Volumes/nur-kanelow/OBI_abstracted_data/Current_Data/data/input/contactlog.csv") 
+  } 
+  
+  # pts that have received pro survey 
+  contact_log_pt = contact_log |>  
+    distinct(patientid)  
+  
+  # select complete survey vars 
+  pro_survey_dt_select = pro_survey_dt |>  
+    select(patientid, survey_informed_consent_b_0)  
+  
+  # merge contact log with pro survey data 
+  pro_survey_sent_complete_dt = contact_log_pt |>  
+    mutate(complete_pro_survey_flg = ifelse(patientid %in% pro_survey_dt_select$patientid, 1, 0)) |> 
+    left_join(pro_survey_dt_select) 
+  
+  # add variable pro_response flag to obi_dt 
+  obi_dt_pro_flg <- obi_dt %>% 
+    filter(patientid %in% pro_survey_sent_complete_dt$patientid) |> 
+    left_join(pro_survey_sent_complete_dt, by = c("patientid")) %>% 
+    select(patientid, site_name, infant_dob_dt, complete_pro_survey_flg, survey_informed_consent_b_0) 
+  
+  if (pro_survey_expired == TRUE) { 
+    obi_dt_pro_flg |> 
+      filter(infant_dob_dt <= pro_expire_date) 
+  } else { 
+    obi_dt_pro_flg 
+  } 
+} 
