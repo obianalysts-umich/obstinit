e1d37d9e3c3825fc6b6830a0bf157172a352615f
abourdea <112494834+abourdea@users.noreply.github.com>
2023-12-07 12:10:56 -0500

    add create_caterpillar_df function

---
+++ R/create_caterpillar_df.R
@@ -0,0 +1,35 @@
+
+#' Create a data frame for caterpillar plots
+#' @description
+#' this function creates a data frame to be used for caterpillar plots where each row is a site name. The df includes a numerator, denominator, and rate for the variable of interest, as well as a 95% CI (bounded by 0 and 1) for the rate.
+#' 
+#' @param df Data frame -- usually this is the data frame read into R using data.table::fread
+#' @param limit_to_locked Logical -- if TRUE, only return cases that are locked
+#' @rdname create_caterpillar_df
+#' @import tidyverse
+#' @export
+
+create_caterpillar_df = function(df, num_var, denom_var, group_var = site_name) {
+  df |>
+    group_by({
+      {
+        group_var
+      }
+    }) |>
+    summarize(
+      denom = sum({
+        {
+          denom_var
+        }
+      }, na.rm = T),
+      num = sum({
+        {
+          num_var
+        }
+      }, na.rm = T),
+      rate = num / denom
+    ) |>
+    obstinit::add_CI_values(rate, denom) |>
+    mutate(LC = ifelse(LC < 0, 0, LC),
+           UC = ifelse(UC > 1, 1, UC))
+}
