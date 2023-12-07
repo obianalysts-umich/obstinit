e2c5256edc918979b906b9cdf26ac57c377d5075
abourdea <112494834+abourdea@users.noreply.github.com>
2023-12-07 13:56:40 -0500

    test add_benchmark function

--- function_testing/test_caterpillar_DEC2023.R
+++ function_testing/test_caterpillar_DEC2023.R
@@ -8,10 +8,13 @@ obi_cohort = obi |>
   obstinit::create_obi_cohort()
 
 test_ces = obi_cohort |> 
-  create_caterpillar_df(cesarean, birth)
+  obstinit::create_caterpillar_df(cesarean, birth)
 
 test_dys = obi_cohort |> 
-  create_caterpillar_df(overall_dystocia_compliance_num, overall_dystocia_den_all)
+  obstinit::create_caterpillar_df(overall_dystocia_compliance_num, overall_dystocia_den_all)
 
-test_ces |> 
-  plot_caterpillar_chart()
+test_plot = test_ces |> 
+  obstinit::plot_caterpillar_chart()
+
+test_plot |> 
+  add_benchmark(y_int = 0.8, add_label = T, label_text = "test", label_xval = 10)
