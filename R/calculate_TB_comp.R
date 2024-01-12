216bce7c9f7eb3412f43ad0b0b65708bcdb21822
Thea B <abourdea@med.umich.edu>
2024-01-12 09:16:32 -0500

    move teambirth variable creation to create_teambirth_cohort

--- R/calculate_TB_comp.R
+++ R/calculate_TB_comp.R
@@ -2,22 +2,12 @@
 #' Calculate TeamBirth compliance
 #'
 #' @export
-#' @param df Data frame -- usually this is the data frame read into R using data.table::fread
+#' @param df Data frame -- note that in order to have the correct variables for numerator and denominator calculation, data frame MUST first be run through obstinit::create_teambirth_cohort
 #' @import tidyverse
 #' @rdname calculate_TB_comp
 
 calculate_TB_comp = function(df) {
   df %>%
-    filter(planned_mode_of_delivery_cd == 1) %>%
-    mutate(
-      huddle_per_12h = ifelse(
-        huddle_doc_b == 1,
-        (huddle_doc_count_no / Admission_minus_delivery) * 12,
-        0
-      ),
-      tb_full_compliance = ifelse(admit_huddle_mtg_compliance == 1 &
-                                    huddle_per_12h >= 1, 1, 0)
-    ) %>%
     summarize(
       TB_num = sum(tb_full_compliance, na.rm = T),
       TB_denom = sum(birth),
