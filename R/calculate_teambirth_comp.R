5d55c1e6dc5eb0dc55b8392e9bb3733e4c51fdb7
Thea B <abourdea@med.umich.edu>
2024-01-12 09:19:12 -0500

    rename calculate_TB_comp -> calculate_teambirth_comp

--- R/calculate_teambirth_comp.R
+++ R/calculate_teambirth_comp.R
@@ -4,9 +4,9 @@
 #' @export
 #' @param df Data frame -- note that in order to have the correct variables for numerator and denominator calculation, data frame MUST first be run through obstinit::create_teambirth_cohort
 #' @import tidyverse
-#' @rdname calculate_TB_comp
+#' @rdname calculate_teambirth_comp
 
-calculate_TB_comp = function(df) {
+calculate_teambirth_comp = function(df) {
   df %>%
     summarize(
       TB_num = sum(tb_full_compliance, na.rm = T),
