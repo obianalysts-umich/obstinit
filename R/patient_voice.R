14ef086b4df95c9101fa7a30a9ceb5e27e2a8d88
Xilin Chen <xilinc@med.umich.edu>
2024-01-31 10:04:18 -0500

    patient voice function text change

--- R/patient_voice.R
+++ R/patient_voice.R
@@ -4,13 +4,12 @@
 #' create patient voice response data 
 #'  
 #' @description 
-#' combine patient contact log with survey response data 
-#' - cohort: pts who received pro survey emails 
-#' - added var:  
-#'    - pro_response: pts who completed pro survey (opt out pts also consider complete) 
-#'    - pts choose to opt out: survey_informed_consent_b_0 ==0 
+#' This function create OBI subset cohort of pts who received pro survey emails.
+#' There are two additional vars added to OBI dataset 
+#'  - pro_response: pts who completed pro survey pro_response == 1 
+#'  - survey_informed_consent_b_0: pts choose to opt out ==0; Self opt out pts also consider complete the survey.
 #' 
-#' @param obi_dt OBI nightly export data
+#' @param obi_dt OBI nightly export data; use current data, becasue expiration date is based on current date
 #' @param pro_survey_expired logical, if TRUE, only include patients who are after 12 weeks postpartum 
 #' 
 #' @return a data frame with variables: patientid, site_name, infant_dob_dt, complete_pro_survey_flg, survey_informed_consent_b_0 
@@ -31,10 +30,6 @@
 
 create_pv_response_dt <- function(obi_dt, 
                                   pro_survey_expired = FALSE) { 
-  # PRO survey starting date 
-  start_date <- lubridate::as_date("2023-5-10") 
-  pro_expire_date <- lubridate::as_date(Sys.Date() - 12 * 7) # 12 weeks postpartum 
-  
   # Patient voice data 
   if (Sys.info()["sysname"] == "Windows") { 
     pro_survey_dt <- data.table::fread("P:/OBI_abstracted_data/Current_Data/data/input/pro_6_week_postpartum.csv") 
@@ -65,6 +60,9 @@ create_pv_response_dt <- function(obi_dt,
     select(patientid, site_name, infant_dob_dt, complete_pro_survey_flg, survey_informed_consent_b_0) 
   
   if (pro_survey_expired == TRUE) { 
+    # PRO survey expiration date 
+    pro_expire_date <- lubridate::as_date(Sys.Date() - 12 * 7) # 12 weeks postpartum 
+    
     obi_dt_pro_flg |> 
       filter(infant_dob_dt <= pro_expire_date) 
   } else { 
