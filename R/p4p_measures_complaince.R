5a0bf1f96720e47ca00cd829b309448ad6b4bfaa
Xilin Chen <xilinc@med.umich.edu>
2024-03-27 17:17:56 -0400

    pv email

--- R/p4p_measures_complaince.R
+++ R/p4p_measures_complaince.R
@@ -1,8 +1,16 @@
 library(obstinit)
+library(tidyverse)
 
 
 obi_dt <- read_current_data(sas_processed_dt = FALSE)
 
-pv_email_submission_rate <- function(obi_dt) {
+pv_email_submission_rate <- function(
+    obi_dt,
+    contact_log_path = "/Volumes/nur-kanelow/OBI_abstracted_data/Current_Data/data/input/contactlog.csv") {
+  # Read contact log, pts with emails are contacted
+  contact_log <- read_csv(contact_log_path)
 
+  # create flg for pts with emails
+  obi_dt <- obi_dt |>
+    mutate(pt_with_emails_flg = ifelse(patientid %in% contact_log$patientid, 1, 0))
 }
