1ae9f4ee89eb08c428563e4615e9ddde60b3fb13
Xilin Chen <xilinc@med.umich.edu>
2024-03-28 14:03:13 -0400

    add family

--- R/p4p_measures_complaince.R
+++ R/p4p_measures_complaince.R
@@ -25,6 +25,8 @@
 #' the reason why we use contact log instead of the variable *email_txt* in the OBI dataset
 #' is that the variable *email_txt* in the OBI dataset was overwritten by data integration early in 2024.
 #' It has been since fixed. But to be save, it's better to use contact log.
+#' 
+#' @family {P4P measures}
 #'
 #' @examples
 #' /dontrun{
@@ -66,9 +68,53 @@ pv_email_submission_rate <- function(
 }
 
 
+#' Calculate race and ethnicity missing measure
+#'
+#' @inheritParams pv_email_submission_rate 
+#'
+#' @details The function filters the input data table to include only records with infant date of birth
+#'          on or after January 1, 2024. It then calculates the number of patients, the number of missing
+#'          race and ethnicity values, and the percentage of missing values. If \code{by_site} is TRUE,
+#'          the measures are calculated separately for each site.
+#'
+#' @examples
+#' /dontrun{
+#' # Calculate race and ethnicity measures for the entire dataset
+#' race_ethnicity_measure(obi_dt)
+#'
+#' # Calculate race and ethnicity measures by site
+#' race_ethnicity_measure(obi_dt, by_site = TRUE)
+#' }
+#'
+#' @family {P4P measures}
+#'
+#' @importFrom dplyr filter summarise
+#' @importFrom cli cli_alert_info
+#' 
+#' @return A summarized data table with the calculated measures.
+#' 
+race_ethnicity_measure <- function(obi_dt,
+                                   by_site = TRUE){
+  obi_dt_gt_2024 = obi_dt |>
+    # race_ethnicity_cd only available after 2024-01-01
+    filter(infant_dob_ymd >= "2024-01-01")
+  
+  cli::cli_alert_info("combined race_ethnicity_cd only available after 2024-01-01.")
 
-race_ethnicity_measure <- function(obi_dt){
-
-
-
+  if (by_site) {
+    obi_dt_gt_2024 |>
+      summarise(
+        n_pt = n(),
+        n_NA_race_ethnicity = sum(is.na(race_ethnicity_cd)),
+        n_missing_pct = round(n_NA_race_ethnicity / n_pt, 2),
+        .by = c(site_name)
+      )
+  } else {
+    obi_dt_gt_2024 |>
+      summarise(
+        n_pt = n(),
+        n_NA_race_ethnicity = sum(is.na(race_ethnicity_cd)),
+        n_missing_pct = round(n_NA_race_ethnicity / n_pt, 2)
+      )
+  }
 }
