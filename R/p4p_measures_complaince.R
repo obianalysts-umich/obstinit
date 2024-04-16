bcbd527bc2c7f2ef9299844edf8eff7d777b35be
Xilin Chen <xilinc@med.umich.edu>
2024-04-16 13:38:35 -0400

    P4P measure - edit email submission rate

--- R/p4p_measures_complaince.R
+++ R/p4p_measures_complaince.R
@@ -5,15 +5,13 @@
 #'
 #' @description
 #' This function calculates the email submission rate  by
-#' comparing the patient IDs in the OBI dataset with the patient IDs in the contact log.
+#' checking if variable `email_txt` has value.
 #' It returns the number of patients, the number of patients who submitted their email,
 #' and the percentage of patients who submitted their email.
 #' compliance definition is at https://docs.google.com/spreadsheets/d/123r8ShMQuxl7GQQsX2Buuh9ynOyMRHOaSeKoFHiwx58/edit#gid=793851872
 #'
 #' @param obi_dt The input dataset containing patient information. If you only need a certain infant dob
 #' range, you can filter the dataset before passing it to this function.
-#' @param contact_log_path The file path to the contact log CSV file. Please
-#' make sure that the contact log file should be from the same date as the OBI dataset.
 #' @param by_site A logical value indicating whether to calculate the email submission rate by site.
 #'
 #' @return A data frame with the following columns:
@@ -21,11 +19,6 @@
 #'   - n_submitted_email: The number of patients who submitted their email.
 #'   - n_submitted_pct: The percentage of patients who submitted their email.
 #'
-#' @details
-#' the reason why we use contact log instead of the variable *email_txt* in the OBI dataset
-#' is that the variable *email_txt* in the OBI dataset was overwritten by data integration early in 2024.
-#' It has been since fixed. But to be save, it's better to use contact log.
-#' 
 #' @family {2024 P4P measures}
 #'
 #' @examples
@@ -33,22 +26,19 @@
 #' obi_dt <- read_current_data(sas_processed_dt = FALSE)
 #' obi_dt_subset <- obi_dt |>
 #'    filter(infant_dob_ymd >= "2023-01-01")
-#' pv_email_submission_rate(obi_dt_subset, contact_log_path = "/path/to/contactlog.csv")
+#' pv_email_submission_rate(obi_dt_subset)
 #' }
 #' 
 #' @export
 
 pv_email_submission_rate <- function(
     obi_dt,
-    contact_log_path = paste0(obstinit::turbo_root_path(), "OBI_abstracted_data/Current_Data/data/input/contactlog.csv"),
     by_site = TRUE) {
-  # Read contact log, pts with emails are contacted
-  contact_log <- read_csv(contact_log_path)
 
   # create flg for pts with emails
   obi_dt_consent <- obi_dt |>
     filter(is.na(pro_opt_out_e)) |>
-    mutate(pt_with_emails_flg = ifelse(patientid %in% contact_log$patientid, 1, 0))
+    mutate(pt_with_emails_flg = ifelse(!is.na(email_txt), 1, 0))
 
   # response rate -----------------------------
   if (by_site) {
