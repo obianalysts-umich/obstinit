90a558b2cdee9928358dbe3d09dca6fb376e62ec
Rebecca Wardrop <rwardrop@umich.edu>
2024-08-20 09:30:06 -0400

    adding htmltools package to lines

--- R/print_obi_report.R
+++ R/print_obi_report.R
@@ -1,19 +1,17 @@
 
-
-
 print_obi_report <- function(input_path, 
                      output_pdf_path, 
                      top_margin = 0.4, 
                      bottom_margin = 0.4,
                      page_number = FALSE,
-                     page_range = NULL) {
+                     page_range = "") {
   
-  footer <- tags$footer(
+  footer <- htmltools::tags$footer(
     style = "width: 100%",
-    img(
+    htmltools::img(
       src = knitr::image_uri("reports/BCBSM BCN_ProcessBlue_B.png"), 
       style = 'display: inline-block; vertical-align: middle; line-height: 1; margin:10px 20px; height: 29px; width: 83.43px'
-    ), div(
+    ), htmltools::div(
       style = "display: inline-block; vertical-align: middle; line-height: 1; font-size: 6pt; width: 670px; font-style: italic; color: #ededed;  font-family: Arial", 
       "Support for the Obstetrics Initiative is provided by Blue Cross Blue Shield of Michigan and Blue Care Network as part of the BCBSM Value Partnerships program. Although Blue Cross Blue Shield of Michigan and the Obstetrics Initiative work collaboratively, the opinions, beliefs and viewpoints expressed by the author do not necessarily reflect the opinions, beliefs and viewpoints of BCBSM or any of its employees."
     )
@@ -21,12 +19,12 @@ print_obi_report <- function(input_path,
   
   
   if (page_number == TRUE) {
-    header <- div(
+    header <- htmltools::div(
       style = "font-size: 6pt; text-align: right; width: 98%; padding-right: 12pt; color: #ededed; font-family: Arial", 
-      span(class = "pageNumber"), "/", span(class = "totalPages")
+      htmltools::span(class = "pageNumber"), "/", htmltools::span(class = "totalPages")
     )
   } else {
-    header <- div(
+    header <- htmltools::div(
       style = "font-size: 6pt; text-align: right; width: 98%; padding-right: 12pt; color: #ededed; font-family: Arial", 
       " "
     )
