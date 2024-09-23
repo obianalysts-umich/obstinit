5a1e80f5326134f462ca9da402fabf20a9e641b1
Thea B. <abourdea@med.umich.edu>
2024-09-23 16:34:42 -0400

    update cat II function to use 2024 flag

--- R/calculate_cat_II_comp.R
+++ R/calculate_cat_II_comp.R
@@ -1,16 +1,16 @@
-
 #' Calculate cat II compliance
 #'
 #' @export
-#' @param df Data frame, run through obstinit::create_cat_II_cohort or otherwise having denominator criteria applied first
+#' @param df Data frame
 #' @import tidyverse
 #' @rdname calculate_cat_II_comp
 
-calculate_cat_II_comp = function(df) {
-  df %>%
+calculate_cat_II_comp <- function(df) {
+  df |>
+    filter(!is.na(cat2_compliance_flg)) |>
     summarize(
-      cat_II_num = sum(ifelse(fht_mgt_documentation_e == 1, 1, 0)),
-      cat_II_denom = sum(birth),
-      cat_II_comp_rate = cat_II_num / cat_II_denom
+      num = sum(cat2_compliance_flg == 1, na.rm = T),
+      denom = n(),
+      rate = mean(cat2_compliance_flg == 1, na.rm = T)
     )
 }
