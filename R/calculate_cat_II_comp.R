6860f7bfa6e41161a8fa588a71a19a4ad2733791
Thea B. <abourdea@med.umich.edu>
2024-09-23 16:42:15 -0400

    revert num, denom, rate var names

--- R/calculate_cat_II_comp.R
+++ R/calculate_cat_II_comp.R
@@ -9,8 +9,8 @@ calculate_cat_II_comp <- function(df) {
   df |>
     filter(!is.na(cat2_compliance_flg)) |>
     summarize(
-      num = sum(cat2_compliance_flg == 1, na.rm = T),
-      denom = n(),
-      rate = mean(cat2_compliance_flg == 1, na.rm = T)
+      cat_II_num = sum(cat2_compliance_flg == 1, na.rm = T),
+      cat_II_denom = n(),
+      cat_II_comp_rate = mean(cat2_compliance_flg == 1, na.rm = T)
     )
 }
