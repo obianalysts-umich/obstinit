5d5b4b1fad6c4749c45228229766ba59a130e82b
Thea B. <abourdea@med.umich.edu>
2025-03-05 09:09:07 -0500

    na.rm for ascensions imputation

--- R/impute_ascension_ces_rates.R
+++ R/impute_ascension_ces_rates.R
@@ -49,23 +49,23 @@ impute_ascension_ces_rates <- function(df, drop_months = T) {
       # average birth volume January - April
       avg_birth_vol = ceiling(rowMeans(across(
         c(birth_vol_month_1:birth_vol_month_4)
-      ))),
+      ), na.rm = T)),
       # average cesarean volume January - April
       avg_ces_vol = ceiling(rowMeans(across(
         c(ces_vol_month_1:ces_vol_month_4)
-      ))),
+      ), na.rm = T)),
       # estimate 2024 birth volume - use observed number Jan - April and July - September, use Jan - April avg for May and June
       birth_vol_24_est = rowSums(across(
         c(birth_vol_month_1:birth_vol_month_4)
-      )) + rowSums(across(c(
+      ), na.rm = T) + rowSums(across(c(
         birth_vol_month_7:{{last_birth_mo}}
-      ))) + (avg_birth_vol * 2),
+      )), na.rm = T) + (avg_birth_vol * 2),
       # estimate 2024 ccesarean volume  - use observed number Jan - April and July - September, use Jan - April avg for May and June
       ces_vol_24_est = rowSums(across(c(
         ces_vol_month_1:ces_vol_month_4
-      ))) + rowSums(across(c(
+      )), na.rm = T) + rowSums(across(c(
         ces_vol_month_7:{{last_ces_mo}}
-      ))) + (avg_ces_vol * 2),
+      )), na.rm = T) + (avg_ces_vol * 2),
       ces_rate_24_est = ces_vol_24_est / birth_vol_24_est
     ) |>
     ungroup()
