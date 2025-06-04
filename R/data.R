47a42b5d89e6e7345b6d46b8737d809bc0dc2521
Rebecca Wardrop <rwardrop@umich.edu>
2025-06-04 09:29:28 -0400

    adding new site

--- R/data.R
+++ R/data.R
@@ -14,7 +14,7 @@
 
 #' Site name list for website upload
 #'
-#' Used in create_website_upload_csv() as a list of site names. Update 11/07/24 to change Ascensions to Henry Fords. Update 2/14/25 to correct Marshfield Dickinson.
+#' Used in create_website_upload_csv() as a list of site names. Update 11/07/24 to change Ascensions to Henry Fords. Update 2/14/25 to correct Marshfield Dickinson. Update 6/4/25 to add McLaren Petoskey.
 #'
 #' @format ## `site_names`
 #' A data frame with 1 column:
@@ -26,7 +26,7 @@
 
 #' Site MDHHS ID list
 #'
-#' Used in create_website_upload_csv() to map site names to MDHHS IDs. Update 11/07/24 to change Ascensions to Henry Fords. Update 2/14/25 to correct Marshfield Dickinson.
+#' Used in create_website_upload_csv() to map site names to MDHHS IDs. Update 11/07/24 to change Ascensions to Henry Fords. Update 2/14/25 to correct Marshfield Dickinson. Update 6/4/25 to add McLaren Petoskey.
 #'
 #' @format ## `site_names`
 #' A data frame with 3 columns:
