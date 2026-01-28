f863be12e7801e3f58a15023f7132224859b32e3
Thea <abourdea@med.umich.edu>
2026-01-28 15:56:17 -0500

    incident case functions

--- R/incident_cases.R
+++ R/incident_cases.R
@@ -3,6 +3,8 @@
 #' @param current_year What year of data is the "current" year?
 #' @import tidyverse
 #' @export
+#' @return A dataframe with one row per site, containing each sites' incident cases comparing the current year to last year. Grouped by MDHHS ID.
+#' @family incident_cases
 
 create_incident_case_site_df <- function(df, current_year) {
   # calculate last year
@@ -39,6 +41,8 @@ create_incident_case_site_df <- function(df, current_year) {
 #' @param current_year What year of data is the "current" year?
 #' @import tidyverse
 #' @export
+#' @return A single number representing the total incident cases across the collaborative. This number is NEGATIVE if there were fewer cesareans this year than last and POSITIVE if there were more cesareans this year than last.
+#' @family incident_cases
 
 calculate_collab_incident_cases <- function(df, current_year) {
   # calculate last year
