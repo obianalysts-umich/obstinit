8ea99e548b450e6b814f546fb1d06ed71190fb58
Rebecca Wardrop <rwardrop@umich.edu>
2026-02-10 09:33:36 -0500

    website upload csv update

--- R/create_website_upload_csv.R
+++ R/create_website_upload_csv.R
@@ -5,21 +5,22 @@
 #' you do not need to re-run this function; you can reuse the previous CSV as long as the report name remains unchanged. If you do NOT want to overwrite previous versions uploaded in the same month, you will need to ensure
 #' the report does not have the same name as the previous version.
 #' 
-#' @param report_name A character string that is the tile of the report excluding the site names. The report filename must be formatted as "[filename] - [sitename]". 
-#' @param tags A character vector that contains the tags for the report. The available tags must be one of the following: Data Request, NTSV Performance Report, NTSV Summary Report, Annual P4P Scorecards, Other, or Site-Specific Patient Voices Resources.
-
+#' @param report_name A character string that is the tile of the report excluding the site names. The report filename must be formatted as "filename - sitename". 
+#' @param tags A character vector that contains the tags for the report. The available tags must be one of the following: Data Request, NTSV Performance Report, NTSV Summary Report, Annual P4P Scorecards, PRIME Reports, Other, or Site-Specific Patient Voices Resources.
 #' @param members_only A logical value that indicates whether the report is for members only. Default is 1.
-#' @param site_year An integer that indicates the year used to determine the sites that have reports. Default is 2025.
-#' @param site_list A data frame that contains the site names. Default is the `site_names` data frame.
-#' @param site_mdhhs A data frame that contains the site names, AMx IDs, and MDHHS IDs. Default is the `site_names_mdhhs` data frame.
-#' @param exclude_site A character vector that contains any sites to exclude. Default is an empty vector.
+#' @param site_list A character vector that contains site names for hospitals with reports.
 #' @param output_path Path to output the CSV file to (must end in /).
 #' 
 #' @return A saved CSV file that can be uploaded to the OBI website.
 #' @examples 
 #' /dontrun{
 #' library(tidyverse)
+#' 
+#' site_list <- c("Michigan Medicine Ann Arbor", "Hurley Medical Center Flint")
+#' 
 #' create_website_upload_csv(report_name = "test report - Michigan Medicine",
+#'                          tags = "NTSV Performance Report",
+#'                           site_list = site_list,
 #'                           output_path = "test_report.csv")
 #' }
 #' 
@@ -29,17 +30,14 @@
 create_website_upload_csv <- function(report_name,
                                       tags,
                                       members_only = 1,
-                                      site_year = 2025,
-                                      site_list = site_names,
-                                      site_mdhhs = site_names_mdhhs,
-                                      exclude_site = c(""),
+                                      site_list,
                                       output_path) {
   
 
   # tags validation --------------------------------------------------------------
   
     if (!(tags %in% c("Data Request", "NTSV Performance Report",  "NTSV Summary Report", "Annual P4P Scorecards", "Other", "Site-Specific Patient Voices Resources"))) {
-      stop("Tags must be one of the following: Data Request, NTSV Performance Report, Weekly Dystocia Report, Annual P4P Scorecards, Other, or Site-Specific Patient Voices Resources.")
+      stop("Tags must be one of the following: Data Request, NTSV Performance Report, Weekly Dystocia Report, Annual P4P Scorecards, PRIME Reports, Other, or Site-Specific Patient Voices Resources.")
     }
     
     if (tags %in% c("P4P Progress Report", "Weekly Dystocia Report")) {
@@ -55,78 +53,21 @@ create_website_upload_csv <- function(report_name,
                           file_url_yr_month_lbl,
                           "/")
   
-  # load data ----------------------------
-  site_names <- site_list
-  site_names_mdhhs <- site_mdhhs
-  
-  obi_dt <- obstinit::read_current_data(sas_processed_dt = FALSE)
-  
-  # workstation site names
-  workstation_site_names <- obi_dt |>
-    dplyr::distinct(site_name, site_id, external_mdhhs_site_id) |>
-    dplyr::rename(workstation_site_name = site_name) 
-  
-  # add IDs to website category data
-  site_names_id <- site_names |>
-    dplyr::left_join(site_names_mdhhs, by = c("site_name" = "site_name"))
-  
-  # manual ID assignment
-  site_names_id_manual <- site_names_id |>
-    dplyr::mutate(
-      site_id_a_mx = dplyr::case_match(
-        site_name,
-        "Munson Healthcare Charlevoix Hospital" ~ 26,
-        "Sparrow Hospital Lansing" ~ 117,
-        "Trinity Health St Mary Mercy Livonia Hospital" ~ 129,
-        "UP Health System Portage Hancock (Level III)" ~ 97,
-        "Trinity Health St Joseph Mercy Oakland Hospital Pontiac" ~ 126,
-        "DMC Hutzel Women's Hospital Detroit" ~ 44,
-        "DMC Sinai Grace Hospital Detroit" ~ 112,
-        "Dickinson County Hospital Iron Mountain" ~ 36,
-        "Hillsdale Hospital Hillsdale" ~ 52,
-        .default = site_id_a_mx
-      )
-    )
-  
-  site_names_id_manual_valid_sites <- site_names_id_manual |>
-    dplyr::filter(!is.na(site_id_a_mx))
-  
-  # merge with workstation data
-  workstation_website_site_name_joined <- site_names_id_manual_valid_sites |>
-    dplyr::left_join(workstation_site_names, by = c("site_id_a_mx" = "site_id"))
-  
-  # identify missed sites
-  workstation_website_site_name_missed <- site_names_id_manual_valid_sites |>
-    dplyr::anti_join(workstation_site_names, by = c("site_id_a_mx" = "site_id"))
-  
-  if (dim(workstation_website_site_name_missed)[1] != 0) {
-    message("Missed sites: ",
-            workstation_website_site_name_missed$site_name)
-  }
-  
-  # only keep sites that have push reports
-  site_with_push_reports <- obi_dt |>
-    dplyr::filter(infant_year == site_year, !site_name %in% exclude_site) |>
-    dplyr::distinct(site_id) |>
-    dplyr::pull()
-  
   # create required columns -----------------------------
-  csv_upload_one_row_per_site <- workstation_website_site_name_joined |>
-    dplyr::filter(site_id_a_mx %in% site_with_push_reports) |>
-    dplyr::select(categories = site_name, workstation_site_name) |>
+  csv_upload_one_row_per_site <- data.frame(site_name = site_list) |>
     dplyr::transmute(
-      title = paste0(yr_month_lbl, " - ", title_lbl, " - ", workstation_site_name),
-      categories,
+      title = paste0(yr_month_lbl, " - ", title_lbl, " - ", site_name),
+      categories = site_name,
       tags = tags,
       members_only = members_only,
       redirect = 0,
       version = "",
       file_date = "",
-      file_urls = paste0(file_url_path, title_lbl, " - ", workstation_site_name, ".pdf")
+      file_urls = paste0(file_url_path, title_lbl, " - ", site_name, ".pdf")
     )
   
   # expand rows with type column; type column has two values: "NTSV" and "NTSV - 2"
-  csv_upload_matched_format <- expand_grid(type = c("download", "version"), csv_upload_one_row_per_site) |>
+  csv_upload_matched_format <- tidyr::expand_grid(type = c("download", "version"), csv_upload_one_row_per_site) |>
     dplyr::arrange(categories) |>
     dplyr::mutate(
       # download row
